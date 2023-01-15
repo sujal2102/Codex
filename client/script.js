929169adb7c47ab12e4ca299b5f4a928b7121416
sujal2102 <72033700+sujal2102@users.noreply.github.com>
2023-01-15 20:00:41 +0530

    update the server url

--- client/script.js
+++ client/script.js
@@ -80,7 +80,7 @@ const handleSubmit = async (e) => {
 
     loader(messageDiv);
 
-    const response = await fetch('https://codex-evcp.onrender.com', {
+    const response = await fetch('https://codex-evcp.onrender.com/', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
